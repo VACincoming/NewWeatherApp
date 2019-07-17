6451839a15a0a83db87b7135f1395975736e9917
VACincoming <alexn98@mail.ru>
2019-07-17 19:04:30 +0300

    starting add multidata

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,49 @@ import { Container, Row, Col } from 'react-bootstrap';
 import "./components/app.css"
 import Particles from 'react-particles-js';
 import { format } from 'date-fns'
+import { string } from 'prop-types';
 
 
 const API_KEY:string = 'f5795a4d5f87e58a619ac306f9d0447d';
 
-interface IState{
-  id?: number,
-  data?: string,
-  city?: string,
-  country?: string,
-  temperature?: string,
-  humidity?: string,
-  pressure?: string,
-  description?: string,
-  clouds?: number,
-  error?: string,
-  date?: string,
-  icon?: string
-}
-
-export default class App extends React.Component<IState> {
-  state: IState = {
-    id: 0,
-    data: '',
-    pressure: '',
-    clouds: 0,
-    temperature: '',
-    city: 'Chernihiv',
-    country: '',
-    humidity: '',
-    description: '',
-    error: '',
-    date: '',
-    icon: ''
+interface IState {
+  // id?: number,
+  // data?: string,
+  // city?: string,
+  // country?: string,
+  // temperature?: string,
+  // humidity?: string,
+  // pressure?: string,
+  // clouds?: number,
+  // error?: string,
+  // icon?: string
+  allData?: ItemOfData[]
   }
+
+  interface ItemOfData {
+    description: string,
+    date: string,
+  }
+
+
+export default class App extends React.Component<{}, IState> {
+  state: any = {
+    allData: [
+      {description: '1', date: '1'},
+      {description: '2', date: '2'},
+    ],
+    // id: 0,
+    // data: '',
+    // pressure: '',
+    // clouds: 0,
+    // temperature: '',
+    // city: 'Chernihiv',
+    // country: '',
+    // humidity: '',
+    // error: '',
+    // icon: ''
+  }
+  
   getInitWeather  = async (city:string, country:string) => {
     const api_call:any = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=metric`)
                             .catch((err) => {
@@ -48,19 +57,18 @@ export default class App extends React.Component<IState> {
       throw new Error(`Could not fetch http://api.openweathermap.org/data/2.5/forecast?q=Chernihiv,ua&appid=${API_KEY}&units=metric , received ${api_call.status}` )
     }
     const data = await api_call.json();
-    console.log(data);
-      this.setState({
-        id: data.city.id,
-        pressure: Math.floor(data.list[0].main.pressure),
-        clouds: data.list[0].clouds.all,
-        temperature: Math.floor(data.list[0].main.temp),
-        city: data.city.name,
-        country: data.city.country,
-        humidity: Math.floor(data.list[0].main.humidity),
-        description: data.list[0].weather[0].description,
-        icon: data.list[0].weather[0].icon,
-        date: format(new Date(data.list[0].dt_txt), 'dd-MM-yyyy hh:mm'),
-        error: ""
+
+    const res = data.list.map((el:any) => ({
+      date: format(new Date(el.dt_txt), 'dd-MM-yyyy hh:mm'),
+      description: el.weather[0].description,
+    }))
+    console.log(res);
+    this.setState({
+      ...this.state,
+      allData: [
+        ...this.state.allData,
+       res,
+      ],
     })
   }
   getWeather = async (e?: any) => {
@@ -79,31 +87,35 @@ export default class App extends React.Component<IState> {
     console.log(data);
     if (city && country) {
       this.setState({
-        id: data.city.id,
-        pressure: Math.floor(data.list[0].main.pressure),
-        clouds: data.list[0].clouds.all,
-        temperature: Math.floor(data.list[0].main.temp),
-        city: data.city.name,
-        country: data.city.country,
-        humidity: Math.floor(data.list[0].main.humidity),
-        description: data.list[0].weather[0].description,
-        icon: data.list[0].weather[0].icon,
-        date: format(new Date(data.list[0].dt_txt), 'dd-MM-yyyy hh:mm'),
-        error: ""
+        // id: data.city.id,
+        // pressure: Math.floor(data.list[0].main.pressure),
+        // clouds: data.list[0].clouds.all,
+        // temperature: Math.floor(data.list[0].main.temp),
+        // city: data.city.name,
+        // country: data.city.country,
+        // humidity: Math.floor(data.list[0].main.humidity),
+        // icon: data.list[0].weather[0].icon,
+        // error: ""
+        allData: [{
+          date: format(new Date(data.list[0].dt_txt), 'dd-MM-yyyy hh:mm'),
+          description: data.list[0].weather[0].description,
+        }]
       });
     } else {
       this.setState({
-        id: undefined,
-        pressure: undefined,
-        clouds: undefined,
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        icon: undefined,
-        date: undefined,
-        error: "Please enter the values."
+        // id: undefined,
+        // pressure: undefined,
+        // clouds: undefined,
+        // temperature: undefined,
+        // city: undefined,
+        // country: undefined,
+        // humidity: undefined,
+        // error: "Please enter the values."
+        // icon: undefined,
+        allData: [{
+          date: '',
+          description: '',
+        }]
       });
     }
   }
@@ -113,7 +125,7 @@ export default class App extends React.Component<IState> {
   public render(){
     return (
       <div>
-        {/* <Particles/> */}
+        {/* <Particles/>git  */}
         <Container className="mainWrapper">
           <Row className="justify-content-center align-items-center">
             <Col xl={{span:3, offset:3}}>
@@ -121,16 +133,15 @@ export default class App extends React.Component<IState> {
             </Col>
             <Col xl={{span:5, offset:1}}>
               <Weather 
-                city={this.state.city!}
-                country={this.state.country!}
-                temperature={this.state.temperature!}
-                humidity={this.state.humidity!}
-                description={this.state.description!}
-                pressure={this.state.pressure!}
-                clouds={this.state.clouds!}
-                error={this.state.error!}
-                date={this.state.date!}
-                icon={this.state.icon!}
+                // city={this.state.city!}
+                // country={this.state.country!}
+                // temperature={this.state.temperature!}
+                // humidity={this.state.humidity!}
+                // pressure={this.state.pressure!}
+                // clouds={this.state.clouds!}
+                // error={this.state.error!}
+                // icon={this.state.icon!}
+                allData={this.state.allData!}
               />
             </Col>
           </Row>
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -3,32 +3,41 @@ import "./weather.css"
 
 
 interface Props {
-  city: string,
-  country: string,
-  temperature: string,
-  humidity: string,
-  description: string,
-  pressure: string,
-  clouds: number,
-  error: string,
-  date: string,
-  icon: string
+  // city: string,
+  // country: string,
+  // temperature: string,
+  // humidity: string,
+  // pressure: string,
+  // clouds: number,
+  // error: string,
+  // icon: string
+  allData: [{
+    description: string | undefined; 
+    date: string | undefined; }];
 }
-
+// city, country, temperature, humidity, description, pressure, clouds, error, icon, 
 export default class Weather extends React.Component<Props>{
   render(){
-    const {city, country, temperature, humidity, description, pressure, clouds, error, icon, date} = this.props
+    const {allData} = this.props;
     return(
-      <div >
-        {description && <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="sunny"/>}
-        {city && this.props.country && <p>Location: {city}, {country}</p>}
-        {date && <p>Date: {date}</p>}
-        {temperature && <p>Temperature: {temperature}&deg;C</p>}
+      <div>
+        {
+          allData.forEach((el:any) => {
+          <p>{el.date}</p>
+        })
+        }
+        {/* <p>{date}</p> */}
+      {/* <p>{description}</p> */}
+        {/* {allDate.date && <p>Date: {allDate.date}</p>} */}
+        {/* {description && <p>Conditions: {description}</p>} */}
+        {/* {description && <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="sunny"/>} */}
+        {/* {city && this.props.country && <p>Location: {city}, {country}</p>} */}
+        {/* {temperature && <p>Temperature: {temperature}&deg;C</p>}
         {humidity && <p>humidity: {humidity}%</p>}
-        {description && <p>Conditions: {description}</p>}
+       
         {pressure && <p>Pressure: {pressure}mm</p>}
         {(clouds || clouds === 0) && <p>clouds: {clouds}%</p>}
-        {error && <p>{error}</p>}
+        {error && <p>{error}</p>} */}
       </div>
     )
   }
